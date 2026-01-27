056f799dc0e851f4bc5da49b5143d836d62dc215
tojik_proof_93 <elementman50@yandex.com>
2026-01-27 22:58:24 +0500

    Support 16 KB page sizes Update Android.mk

--- native-entrypoint/src/main/jni/Android.mk
+++ native-entrypoint/src/main/jni/Android.mk
@@ -1,6 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
+
 LOCAL_MODULE:= libnative-entrypoint
 LOCAL_SRC_FILES:= main.cpp
 LOCAL_LDLIBS:= -llog -landroid -ldl
+
+LOCAL_LDFLAGS += "-Wl,-z,max-page-size=16384"
+
 include $(BUILD_SHARED_LIBRARY)
