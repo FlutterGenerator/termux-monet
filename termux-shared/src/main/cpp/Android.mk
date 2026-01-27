7da465c4125f9d73345851e46fd5393c08bcab38
tojik_proof_93 <elementman50@yandex.com>
2026-01-27 23:02:10 +0500

    Update Android.mk

--- termux-shared/src/main/cpp/Android.mk
+++ termux-shared/src/main/cpp/Android.mk
@@ -1,6 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
-LOCAL_LDLIBS := -llog
+
 LOCAL_MODULE := local-socket
 LOCAL_SRC_FILES := local-socket.cpp
+LOCAL_LDLIBS := -llog
+
+LOCAL_LDFLAGS += "-Wl,-z,max-page-size=16384"
+
 include $(BUILD_SHARED_LIBRARY)
