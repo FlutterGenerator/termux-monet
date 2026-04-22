3e2786f977f65679c1dd1f5894641ea862e2c646
tojik_proof_93 <elementman50@yandex.com>
2026-04-22 23:39:30 +0500

    Update Android.mk

--- native-entrypoint/src/main/jni/Android.mk
+++ native-entrypoint/src/main/jni/Android.mk
@@ -3,4 +3,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE:= libnative-entrypoint
 LOCAL_SRC_FILES:= main.cpp
 LOCAL_LDLIBS:= -llog -landroid -ldl
+LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384
 include $(BUILD_SHARED_LIBRARY)
